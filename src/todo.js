a16b89dcc8691ada9aebfd26ab918cae9b8c23a7
Nate <for.education.n@yandex.ru>
2021-06-29 22:31:51 +0200

    Style WIP

---
+++ src/todo.js
@@ -0,0 +1,11 @@
+const todoFactory = (title, description, dueDate, priority) => {
+  //   const getTitle = () => {
+  //     return title;
+  //   };
+  //   const getDescription = () => description;
+  //   const getDueDate = () => dueDate;
+  //   const getPriority = () => priority;
+  //   return { getTitle, getDescription, getDueDate, getPriority };
+};
+
+// const testTodo = todoFactory("Test TODO", "Todo to test", 10, "low");
